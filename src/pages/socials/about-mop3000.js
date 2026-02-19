9fb13710188120f5a23333575e0ae62311ce9014
Gvbvdxx (Alt Account) <gvbvdxxalt2@outlook.com>
2026-02-19 12:05:51 -0500

    Update about-mop3000.js

--- src/pages/socials/about-mop3000.js
+++ src/pages/socials/about-mop3000.js
@@ -7,8 +7,11 @@ const USERNAME = "MOP 3000";
 const ALT_NAME = "Username in Scratch: Im_CatmanYT"; //MOP3000 doesn't prefer using real name so use scratch name.
 
 const DESCRIPTION = [
-  "I met him in real life. Came up with the idea of Random Rants.",
-  "Is a 2D animator and obsessed with animation.",
+  "I met him in real life, and he’s the original spark behind Random Rants. He’s the one who first pitched the idea for the platform.",
+  "He acts as our Lead Creative Director, coming up with most of the site's unique features and 'goofy' admin commands.",
+  "When he’s not brainstorming new ideas for the chat, he’s a dedicated 2D animator with over 200 projects on Scratch.",
+  "He handles our QA and testing—if the mobile version is acting up or a command needs balancing, he’s usually the first to spot it.",
+  "From 'Cheese Storms' to 'Australian Mode,' he’s the mind behind the personality and fun that makes this project what it is."
 ];
 
 const SOCIAL_LINKS = [
